3276097a11aa77620e817571960a4c2f10be20ef
ttytango <59534146+ttytango@users.noreply.github.com>
2020-10-26 20:14:55 +0000

    file creation altered to append mode to prevent overwriting files

--- directories.py
+++ directories.py
@@ -1,27 +1,12 @@
-# scss_dirs = {'abstracts': ["_functions.scss", "_mixins.scss", "_variables.scss"],
-#              'base': {"_animations.scss", "_base.scss", "_typography.scss", "_utilities.scss"},
-#              'components': ["_button.scss", "_cards.scss", "_modal.scss", "_navigation.scss"],
-#              'layouts': ["footer.scss", "_grid.scss", "_header.scss", "_main.scss", "_subheader.scss"],
-#              'pages': ["index.scss", "_contacts.scss", "_about.scss"]}
-#
 
-scss = ["abstracts", "base", "components", "layouts", "pages"]
+"""Add or remove directories here within the square brackets to taste, or add any extra directories you
+    want on a new line within dir_dict using the same format as below """
 
 dir_dict = {
     "abstracts": ["_functions.scss", "_mixins.scss", "_variables.scss"],
     "base": ["_animations.scss", "_base.scss", "_typography.scss", "_utilities.scss"],
-    "components": ["_button.scss", "_cards.scss", "_modal.scss", "_navigation.scss"],
+    "components": ["_button.scss", "_cards.scss", "_carousel.scss", "_modal.scss", "_navigation.scss"],
     "layouts": ["_footer.scss", "_grid.scss", "_header.scss", "_main.scss", "_subheader.scss"],
-    "pages": ["_index.scss", "_contacts.scss", "_about.scss"]
-}
+    "pages": ["_index.scss", "_contacts.scss", "_about.scss"],
 
-# 
-# """ 
-# Add/remove filenames here within the square brackets to taste
-# """
-#
-# abstracts = ["_functions.scss", "_mixins.scss", "_variables.scss"]
-# base = ["_animations.scss", "_base.scss", "_typography.scss", "_utilities.scss"]
-# components = ["_button.scss", "_cards.scss", "_modal.scss", "_navigation.scss"]
-# layouts = ["_footer.scss", "_grid.scss", "_header.scss", "_main.scss", "_subheader.scss"]
-# pages = ["_index.scss", "_contacts.scss", "_about.scss"]
+}
