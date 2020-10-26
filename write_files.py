6d048cdacebfeba428a8c14ca25d6450ac6506ba
Dave Birch <dave.birch@gmail.com>
2020-10-26 18:55:26 +0000

    Refactor to use PathLib and programatically iterate the dict in directories.py

--- write_files.py
+++ write_files.py
@@ -2,15 +2,9 @@
 """
 This is written for python 3.8.x rather than for python 3.9 Python 3.9's now contains a built-in PEP 616 method
 to remove prefix and suffixes Python 3.9 will not work with windows 7 or earlier however, so I have stuck to
-regular expressions for the time being. https://www.python.org/dev/peps/pep-0616/ :rtype: object
+regular expressions for the time being. https://www.python.org/dev/peps/pep-0616/
+:rtype: object
 """
-
-
-def remove_affix(word, folder, prefix="_", suffix=".scss"):
-    for w in range(0, len(folder)):
-        if word.startswith(prefix) and word.endswith(suffix):
-            return word[len(prefix):-len(suffix)]
-    return word
 
 
 def endswith(self, suffix):
@@ -19,5 +13,15 @@ def endswith(self, suffix):
 
 def startswith(self, prefix):
     pass
+
+
+def remove_affix(word, prefix="_", suffix=".scss"):
+    if word.startswith(prefix) and word.endswith(suffix):
+        return word[len(prefix):-len(suffix)]
+
+    return word
+
+
+
 
 
